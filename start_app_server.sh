3cf3370acb80be83b34d75205d1a92c260c5b8ae
Robin Cher <robin_cher@ida-gds.com>
2018-02-20 10:41:15 +0800

    Added Mongo Credentials and set it as env var

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://test:testtest@ds239638.mlab.com:39638/devseccon2018
+export MONGODB_CREDENTIALS mongodb://<dbuser>:<dbpassword>@ds241658.mlab.com:41658/devsecconsg
 
 # finally, run the app in dev mode...
 npm run dev
