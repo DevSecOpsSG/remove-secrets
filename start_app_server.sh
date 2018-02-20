5e0dd95b1f91b38039bb7e61b627c28a05006cb3
Robin Cher <robin_cher@ida-gds.com>
2018-02-20 11:03:16 +0800

    Fix typo

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS mongodb://bccher:password@ds241658.mlab.com:41658/devsecconsg
+export MONGODB_CREDENTIALS=mongodb://bccher:password@ds241658.mlab.com:41658/devsecconsg
 
 # finally, run the app in dev mode...
 npm run dev
