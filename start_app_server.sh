7c28b6d5fe99f64e87c7d60406a2a4934a9e60c5
Fabian Lim <fabianlim1989@gmail.com>
2018-08-13 21:53:31 +0800

    Update start_app_server.sh

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://test:testtest@ds239638.mlab.com:39638/devseccon2018
+export MONGODB_CREDENTIALS=mongodb://{{USER}}:{{PASSWORD}}@ds666666.mlab.com:39999/some-database
 
 # finally, run the app in dev mode...
 npm run dev
